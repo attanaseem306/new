cb5cf255d2de80a3d653dbfb266406d5c4e55dd4
MRMutahir <mutahirkareem820@example.com>
2023-08-26 18:39:05 +0500

    third

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import ButtonAppBar from './card';
+import SimpleSlider from './slider';
+import React from 'react';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar/>
+      <SimpleSlider/>
+
+    </div>
+  )
+}
+
+export default App;
